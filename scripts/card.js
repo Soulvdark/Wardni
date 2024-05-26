ffee6822a4e96832d45d472dc3c25afb47c29ebe
Pow_point <88034480+Soulvdark@users.noreply.github.com>
2024-05-26 03:32:06 +0300

    Add files via upload

---
+++ scripts/card.js
@@ -0,0 +1,93 @@
+const initialCards = [
+	{
+		link: 'https://im.wampi.ru/2024/05/23/grem.png',
+		use: 'https://i1.wampi.ru/2024/05/25/grem-radio.png'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/23/flash.png',
+		use: 'https://im.wampi.ru/2024/05/26/lush_firefly.gif'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/23/huggy.png',
+		use: 'https://im.wampi.ru/2024/05/26/huggy_wuggy_mini.gif'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/23/book__men.png',
+		use: 'https://i1.wampi.ru/2024/05/25/book.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/23/boxy__boo.png',
+		use: 'https://i1.wampi.ru/2024/05/26/boxy_boo_mini.gif'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/23/drone.png',
+		use: 'https://i1.wampi.ru/2024/05/26/drone_magic.js22.gif'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/23/cat.png',
+		use: 'https://im.wampi.ru/2024/05/25/cat.png'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/23/magnum.png',
+		use: 'https://im.wampi.ru/2024/05/26/gun.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/23/radion.png',
+		use: 'https://i1.wampi.ru/2024/05/25/grem-radio.png'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/23/egg.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/23/info__board.png'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/23/boxs.png'
+	}
+];
+const initialCards__medium = [
+	{
+		link: 'https://im.wampi.ru/2024/05/24/pos.png',
+		use: 'https://i1.wampi.ru/2024/05/25/pos.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/24/knight.png',
+		use: 'https://im.wampi.ru/2024/05/26/knight.gif'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/24/lizard.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/24/stalker.png',
+		use: 'https://i1.wampi.ru/2024/05/26/stalker.gif'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/24/be__drone.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/24/farmer__drone.png'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/24/drone__kills.png',
+		use: 'https://im.wampi.ru/2024/05/25/killer__boots.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/24/cinema__man.png'
+	},
+	{
+		link: 'https://i1.wampi.ru/2024/05/24/modest.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/25/tentak.png',
+		use: 'https://im.wampi.ru/2024/05/26/tentacles2.gif'
+	}
+];
+const initialCards__big = [
+	{
+		link: 'https://i1.wampi.ru/2024/05/24/card.png'
+	},
+	{
+		link: 'https://im.wampi.ru/2024/05/24/big__model.png',
+		use: 'https://i1.wampi.ru/2024/05/26/kal.png'
+	}
+];
--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,77 @@ const popup__close = document.querySelector('.close__popup');
 const img__active = document.querySelector('.img__animation');
 const bodyElement = document.body;
 
-img__active.addEventListener('click', () => {
+const photos__small = document.querySelector('.photo__grid-small');
+const photos__medium = document.querySelector('.photo__grid-medium');
+const photos__big = document.querySelector('.photo__grid-big');
+const cardTemplate = document.querySelector('#card-template').content;
+// const cardImage = cardElement.querySelector('.portfolio__photos');
+
+const popupImage = document.querySelector('.popup__img-full');
+const popupImageOpenedPicture = popupImage.querySelector(
+	'.popup__img__full-screenshot'
+);
+const popupImageUsed = popupImage.querySelector(
+	'.popup__full-screenshot__usage'
+);
+const popupImageOpenedText = popupImage.querySelector('.popup__img-full__text');
+
+function addCard(link, use) {
+	const imgGroup = popupImage.querySelector('.popup__group');
+	const cardElement = cardTemplate
+		.querySelector('.img__animation')
+		.cloneNode(true);
+	const cardImg = cardElement.querySelector('.portfolio__photos');
+	cardElement.querySelector('.portfolio__photos').src = link;
+	cardElement.querySelector('.img__used').src = use;
+	cardImg.addEventListener('click', function (event) {
+		imgOpen(event);
+	});
+	return cardElement;
+}
+for (let i = 0; i < initialCards.length; i++) {
+	photos__small.append(addCard(initialCards[i].link, initialCards[i].use));
+}
+for (let i = 0; i < initialCards__medium.length; i++) {
+	photos__medium.append(
+		addCard(initialCards__medium[i].link, initialCards__medium[i].use)
+	);
+}
+for (let i = 0; i < initialCards__big.length; i++) {
+	photos__big.append(
+		addCard(initialCards__big[i].link, initialCards__big[i].use)
+	);
+}
+
+function imgOpen(event) {
+	const img = event.target.closest('.img__animation');
+	popupImageOpenedPicture.src = event.target.src;
+	popupImageUsed.src = img.querySelector('.img__used').src;
 	popup.classList.add('popup__active');
 	bodyElement.style.overflow = 'hidden';
-});
-
+}
 popup__close.addEventListener('click', () => {
 	popup.classList.remove('popup__active');
-    bodyElement.style.overflow = 'scroll';
+	bodyElement.style.overflow = 'scroll';
 });
+
+// function openPopup(popup) {
+// 	popup.classlist.add('popup__active');
+// }
+// cardImage.addEventListener('click', function (event) {
+// 	scaleImage(event);
+// });
+
+// function scaleImage(event) {
+// 	const card = event.target.closest('.img__animation');
+// 	popupImageOpenedPicture.src = event.target.src;
+// 	// popupImageOpenedText.textContent =
+// 	// 	card.querySelector('.card__title').textContent;
+// 	popup.classList.add('popup__active');
+// 	bodyElement.style.overflow = 'hidden';
+// }
+
+// popup__close.addEventListener('click', () => {
+// 	popup.classList.remove('popup__active');
+// 	bodyElement.style.overflow = 'scroll';
+// });
